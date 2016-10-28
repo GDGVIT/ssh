09ebaf341a1577f72b3aad1fdcd96f24fef4b29b
DefaltSimon <simon.peter.goricar@gmail.com>
2016-10-28 20:14:49 +0200

    PEP8 and code optimization

--- receiver.py
+++ receiver.py
@@ -1,19 +1,27 @@
-__author__ = 'Ujjwal'
-
+# coding=utf-8
 import subprocess
 import os
 import signal
 import socket
-from SSHconfigure import sshConfig
+from SSHconfigure import configure_ssh
+
+__author__ = 'Ujjwal'
+
+host = ""
+port = 9999
+s = None
+F_NULL = open(os.devnull, 'w')
 
 try:
     __USER__ = os.environ.copy()['SUDO_USER']
     if __USER__ == 'root':
         __USER__ = '../root'
-except:
-    print('Run as sudo')
+
+except KeyError:
+    print('Run as root')
     exit(0)
-FNULL = open(os.devnull, 'w')
+
+# Functions
 
 
 def socket_create():
@@ -21,9 +29,11 @@ def socket_create():
         global host
         global port
         global s
+
         host = ''
         port = 9999
         s = socket.socket()
+
     except socket.error:
         print("Socket creation error")
 
@@ -33,61 +43,83 @@ def socket_bind():
         global host
         global port
         global s
+
         s.bind((host, port))
         s.listen(5)
+
     except socket.error:
         print("Socket binding error")
         exit(0)
 
 
-def recvfile():
-    if ('sshd' not in str(subprocess.check_output(['ls', '/usr/sbin']))):
-        sshConfig()
-    if ('arcfour' not in str(subprocess.check_output(['cat', '/etc/ssh/sshd_config']))):
-        sshConfig()
-    sshServ = subprocess.Popen(['sudo', '/usr/sbin/sshd', '-p', '2222', '-D'], preexec_fn=os.setsid)
+def receive_file():
+    if 'sshd' not in str(subprocess.check_output(['ls', '/usr/sbin'])):
+        configure_ssh()
+    if 'arcfour' not in str(subprocess.check_output(['cat', '/etc/ssh/sshd_config'])):
+        configure_ssh()
+
+    ssh_server = subprocess.Popen(['sudo', '/usr/sbin/sshd', '-p', '2222', '-D'], preexec_fn=os.setsid)
+
+    # Creates and binds the socket
     socket_create()
     socket_bind()
+
+    # Waits for a connection
     print('Waiting...')
     try:
         conn, address = s.accept()
     except (KeyboardInterrupt, EOFError):
-        print(' Keyboard Interrupt')
-        os.killpg(os.getpgid(sshServ.pid), signal.SIGTERM)
+        # Kill sshd on KeyboardInterrupt or EOFError
+        print('Keyboard Interrupt')
+        os.killpg(os.getpgid(ssh_server.pid), signal.SIGTERM)
         return
+
     print(str(conn.recv(1024), encoding='utf-8'))
+
+    # Confirmation
     confirmation = input("Do you want to accept the connection? ")
-    if confirmation == 'y' or confirmation == 'Y':
+    if str(confirmation).lower() == "y":
         conn.send(str.encode("Yes"))
     else:
         conn.close()
-        os.killpg(os.getpgid(sshServ.pid), signal.SIGTERM)
+        os.killpg(os.getpgid(ssh_server.pid), signal.SIGTERM)
         return
+
+    # Get the public key
     public_key = str(conn.recv(2048), encoding='utf-8')
 
-    if (subprocess.call(['cp', '/home/' + __USER__ + '/.ssh/authorized_keys', '/home/' + __USER__ + '/.ssh/authorized_keys_Backup'], stdout=FNULL,
-                        stderr=subprocess.STDOUT)):
-        subprocess.call(['touch', '/home/' + __USER__ + '/.ssh/authorized_keys_Backup'])
+    # .ssh/authorized_keys already exists, make an empty backup file
+    if (subprocess.call(['cp', "/home/{}/.ssh/authorized_keys".format(__USER__), "/home/{}/.ssh/authorized_keys_Backup".format(__USER__)],
+                        stdout=F_NULL, stderr=subprocess.STDOUT)):
 
+        subprocess.call(['touch', "/home/{}/.ssh/authorized_keys_Backup".format(__USER__)])
+
+    # Add the received key to .ssh/authorized_keys
     with open('/home/' + __USER__ + '/.ssh/authorized_keys', 'a') as f:
         f.write(public_key)
+
     try:
+        # Send the user name and close the socket
         conn.send(str.encode(__USER__.lstrip('../')))
         conn.recv(256)
         conn.close()
-        os.remove('/home/' + __USER__ + '/.ssh/authorized_keys')
-        os.rename('/home/' + __USER__ + '/.ssh/authorized_keys_Backup', '/home/' + __USER__ + '/.ssh/authorized_keys')
-        os.killpg(os.getpgid(sshServ.pid), signal.SIGTERM)
-    except:
+
+    except socket.error:
+        # Abandon the connection
         try:
             conn.close()
-        except:
+        except socket.error:
             pass
-        os.remove('/home/' + __USER__ + '/.ssh/authorized_keys')
-        os.rename('/home/' + __USER__ + '/.ssh/authorized_keys_Backup', '/home/' + __USER__ + '/.ssh/authorized_keys')
-        os.killpg(os.getpgid(sshServ.pid), signal.SIGTERM)
+
         print('Error encountered')
+    finally:
+        # Remove the public key file and revert it back to previous state
+        os.remove("/home/{}/.ssh/authorized_keys".format(__USER__))
+        os.rename("/home/{}/.ssh/authorized_keys_Backup".format(__USER__), "/home/{}/.ssh/authorized_keys".format(__USER__))
+
+        # Kill the sshd server
+        os.killpg(os.getpgid(ssh_server.pid), signal.SIGTERM)
 
 
 if __name__ == '__main__':
-    recvfile()
+    receive_file()
--- scanner.py
+++ scanner.py
@@ -1,30 +1,32 @@
-__author__ = 'Ujjwal'
-
+# coding=utf-8
 import socket
 import threading
 import ipaddress
 import subprocess
 
-__PORT__ = 2222
-__DELAY__ = 2
+
+__author__ = 'Ujjwal'
+
+PORT = 2222
+DELAY = 2
 
 
-def TCP_connect(ip, port_number, delay, hosts):
-    host_name='Unknown'
-    TCPsock = socket.socket()
-    TCPsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    TCPsock.settimeout(delay)
+def tcp_connect(ip, port_number, delay, hosts):
+    host_name = 'Unknown'
+
+    # Create the socket
+    tcp_socket = socket.socket()
+    tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcp_socket.settimeout(delay)
+
+    # Connect
     try:
         host_name = socket.gethostbyaddr(ip)[0]
-        #print(host_name)
-    except:
-        pass
-    try:
-        TCPsock.connect((ip, port_number))
-        if 'SSH' in str(TCPsock.recv(256)):
-            hosts.append((host_name,ip))
-        #print(host_name,ip)
-    except:
+
+        tcp_socket.connect((ip, port_number))
+        if 'SSH' in str(tcp_socket.recv(256)):
+            hosts.append((host_name, ip))
+    except socket.error:
         pass
 
 
@@ -32,7 +34,7 @@ def scan_ports():
     threads = []
     hosts = []
 
-    network=str(subprocess.check_output(['ip','route']),encoding='utf-8').split('\n')[-2].split()[0]
+    network = str(subprocess.check_output(['ip', 'route']), encoding='utf-8').split('\n')[-2].split()[0]
     if '/' not in network:
         print('Could not determine gateway')
         return
@@ -47,24 +49,27 @@ def scan_ports():
     prefix_len=ipaddress.IPv4Network(mask).prefixlen
     #print(prefix_len)
     """
+
     print("Scanning...")
+    # Iterate though hosts (each one in a thread)
     for host_ip in ipaddress.IPv4Network(network):
-        #print(host_ip)
-        t = threading.Thread(target=TCP_connect, args=(str(host_ip), __PORT__, __DELAY__, hosts))
+        # Create a thread and add it to the list
+        t = threading.Thread(target=tcp_connect, args=(str(host_ip), PORT, DELAY, hosts))
         threads.append(t)
-        if len(threads)== 255:
+
+        if len(threads) == 255:
             for t in threads:
                 t.start()
             for t in threads:
                 t.join()
-            threads=[]
-    if(threads !=[]):
+            threads = []
+
+    if threads:
         for t in threads:
             t.start()
         for t in threads:
             t.join()
-    #for host in hosts:
-    #    print(host)
+
     return hosts
 
 
