6794232fbb68f7e9823ac4be90427d5d29ce81c8
Ujjwal Verma <ujjwalverma1111@gmail.com>
2017-02-08 22:41:29 +0530

    Minor changes

--- utils/SSHconfigure.py
+++ utils/SSHconfigure.py
@@ -1,18 +1,18 @@
-import subprocess
 import os
+from shutil import copyfile
+import subprocess
+import sys
 
 FNULL = open(os.devnull, 'w')
 
 
 def ssh_config():
     subprocess.call(['sudo', 'apt-get', 'install', 'openssh-server', '-y'], stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call(['sudo', 'cp', '/etc/ssh/sshd_config', '/etc/ssh/sshd_config.factory-defaults'], stdout=FNULL,
-                    stderr=subprocess.STDOUT)
-    if subprocess.call(['sudo', 'service', 'sshd', 'stop'], stdout=FNULL, stderr=subprocess.STDOUT):
+    if not os.path.exists('/usr/sbin/sshd'):
         print('Could not install ssh server. Check your repositories.')
-        exit(0)
-    subprocess.call(['sudo', 'cp', '/etc/ssh/fireshare', '/etc/ssh/sshd_config.factory-defaults'], stdout=FNULL,
-                    stderr=subprocess.STDOUT)
+        sys.exit(0)
+    copyfile('/etc/ssh/sshd_config', '/etc/ssh/sshd_config.factory-defaults')
+    copyfile('/etc/ssh/sshd_config.factory-defaults', '/etc/ssh/fireshare')
     lines = [
         "Ciphers 3des-cbc,aes128-cbc,aes192-cbc,aes256-cbc,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,"
         "aes256-gcm@openssh.com,arcfour,arcfour128,arcfour256,blowfish-cbc\n",
